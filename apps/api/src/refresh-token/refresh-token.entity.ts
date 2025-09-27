c55453e2a421bb4d04dfc82cddb18c130a0ccc95
harryplusplus <harryplusplus@gmail.com>
2025-09-28 00:38:17 +0900

    Refactor schema

--- apps/api/src/refresh-token/refresh-token.entity.ts
+++ apps/api/src/refresh-token/refresh-token.entity.ts
@@ -42,10 +42,7 @@ export const RefreshTokenSchema = new EntitySchema<RefreshToken>({
     userId: { type: "bigint" },
   },
   relations: {
-    user: {
-      type: "many-to-one",
-      target: UserSchema.options.name,
-    },
+    user: { type: "many-to-one", target: UserSchema.options.name },
   },
   indices: [
     {
